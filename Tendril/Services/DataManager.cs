8cd19c861852040413fdb547df801f63ab887c5c
ARES\Impactus <alex.l.harper@outlook.com>
2022-06-10 15:16:32 -0700

    Added Document comments to the DataManager class

--- Tendril/Services/DataManager.cs
+++ Tendril/Services/DataManager.cs
@@ -7,6 +7,9 @@ using Tendril.Models;
 using Tendril.Services.Interfaces;
 
 namespace Tendril.Services {
+	/// <summary>
+	/// Service class which provides a CRUD interface to registered data collections<br />
+	/// </summary>
 	public class DataManager : IDataManager {
 		internal Dictionary<Type, object> TDataSourceToDataSourceContext { get; }
 		internal Dictionary<Type, IDataCollection> TModelToCollectionContext { get; }
@@ -16,6 +19,17 @@ namespace Tendril.Services {
 			TModelToCollectionContext = new Dictionary<Type, IDataCollection>();
 		}
 
+		/// <summary>
+		/// Insert a model into the data collection<br />
+		/// <b>Example Usage:</b><br />
+		/// <code>
+		/// // Create a new student with the name <b>John Doe</b><br />
+		/// var student = await dataManager.Create( new Student { Name = "John Doe" } );
+		/// </code>
+		/// </summary>
+		/// <typeparam name="TModel">The type of model</typeparam>
+		/// <param name="source">The model to be inserted</param>
+		/// <returns>The model that has been inserted</returns>
 		public async Task<TModel> Create<TModel>( TModel source ) where TModel : class {
 			var collectionContext = GetCollectionContext<TModel>();
 			using var dataContext = collectionContext.GetDataContext();
@@ -24,6 +38,21 @@ namespace Tendril.Services {
 			return entity;
 		}
 
+		/// <summary>
+		/// Perform a bulk insert into the data collection<br />
+		/// <b>Example Usage:</b><br />
+		/// <code>
+		/// // Create 2 new students with the names <b>John Doe</b> and <b>Jane Doe</b><br />
+		/// var students = new List&lt;Student&gt; {
+		///		new Student { Name = "John Doe" },
+		///		new Student { Name = "Jane Doe" }
+		///	};
+		/// await dataManager.CreateRange( students, 2 );
+		/// </code>
+		/// </summary>
+		/// <typeparam name="TModel">The type of model</typeparam>
+		/// <param name="source">The collection of models to be inserted</param>
+		/// <param name="batchSize">The number of models to insert at a time</param>
 		public async Task CreateRange<TModel>( IEnumerable<TModel> source, int batchSize ) where TModel : class {
 			var collectionContext = GetCollectionContext<TModel>();
 			using var dataContext = collectionContext.GetDataContext();
@@ -33,6 +62,20 @@ namespace Tendril.Services {
 			}
 		}
 
+		/// <summary>
+		/// Perform an update on a given model
+		/// <br />
+		/// <b>Example Usage:</b><br />
+		/// <code>
+		/// // Insert a student and then change their name<br />
+		/// var student = await dataManager.Create( new Student { Name = "John Doe" } );
+		/// student.Name = "John Lee Doe";
+		/// await dataManager.Update( student );
+		/// </code>
+		/// </summary>
+		/// <typeparam name="TModel">The type of model</typeparam>
+		/// <param name="source">The model to be updated</param>
+		/// <returns>The model that has been updated</returns>
 		public async Task<TModel> Update<TModel>( TModel source ) where TModel : class {
 			var collectionContext = GetCollectionContext<TModel>();
 			using var dataContext = collectionContext.GetDataContext();
@@ -41,6 +84,15 @@ namespace Tendril.Services {
 			return entity;
 		}
 
+		/// <summary>
+		/// Delete a model from the data collection<br />
+		/// <b>Example Usage:</b><br />
+		/// <code>
+		/// await dataManager.Delete( student );
+		/// </code>
+		/// </summary>
+		/// <typeparam name="TModel">The type of model</typeparam>
+		/// <param name="source">The model to be deleted</param>
 		public async Task Delete<TModel>( TModel source ) where TModel : class {
 			var collectionContext = GetCollectionContext<TModel>();
 			using var dataContext = collectionContext.GetDataContext();
@@ -48,6 +100,24 @@ namespace Tendril.Services {
 			await collectionContext.SaveChangesAsync( dataContext );
 		}
 
+		/// <summary>
+		/// <b>Example Usage:</b><br />
+		/// <code>
+		/// // Get all students who's names start with the letter <b>A</b> or <b>B</b><br />
+		/// var students = dataManager.FindByFilter&lt;Student&gt;(
+		///		new OrFilterChip(
+		///			new FilterChip( "Name", FilterOperator.StartsWith, "A" ),	
+		///			new FilterChip( "Name", FilterOperator.StartsWith, "B" )
+		///		)
+		/// );
+		/// </code>
+		/// </summary>
+		/// <typeparam name="TModel"></typeparam>
+		/// <param name="filter"></param>
+		/// <param name="page"></param>
+		/// <param name="pageSize"></param>
+		/// <returns>Resulting dataset</returns>
+		/// <exception cref="UnsupportedFilterException"></exception>
 		public async Task<IEnumerable<TModel>> FindByFilter<TModel>(
 			FilterChip filter = null,
 			int? page = null,
@@ -62,6 +132,23 @@ namespace Tendril.Services {
 			return result.Data;
 		}
 
+		/// <summary>
+		/// Execute a raw query against the underlying data collection<br />
+		/// <b>Note:</b>This operation should only be performed for performance critical code,<br />
+		/// or if the required operation is not possible using the other CRUD methods, as this exposes implementation details<br />
+		/// <b>Example Usage:</b><br />
+		/// <code>
+		/// // Using SQL, find all students who's <b>Name</b> contains the substring <b>Lee</b><br />
+		/// var students = await dataManager.ExecuteRawQuery&lt;Student&gt;(
+		///		@"SELECT * FROM Students s WHERE s.Name LIKE '%' + @name + '%'",
+		///		new MySqlParameter( "name", "Lee" )
+		/// );
+		/// </code>
+		/// </summary>
+		/// <typeparam name="TModel">The type of model</typeparam>
+		/// <param name="query">The raw query to execute</param>
+		/// <param name="parameters">params style array of query parameters to use</param>
+		/// <returns>Resulting dataset</returns>
 		public async Task<IEnumerable<TModel>> ExecuteRawQuery<TModel>( string query, params object[] parameters ) where TModel : class {
 			var collectionContext = GetCollectionContext<TModel>();
 			using var dataContext = collectionContext.GetDataContext();
